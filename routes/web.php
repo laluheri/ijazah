d6fb147684a5292a6fadf1d40ce675da5b37eb80
laluheri <heri170890@gmail.com>
2025-07-02 23:20:11 +0800

    generate app key route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PDFController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PDFController::class, 'index']);
 Route::post('/upload', [PDFController::class, 'upload']);
+
+Route::get('/generate-key', function () {
+    Artisan::call('key:generate');
+    return 'APP_KEY generated!';
+});
